d1fdbe18a2c3311e2979a5b9300637bfab8e6e28
David Sanchez <davidalejandrosanchezarias058@gmail.com>
2022-05-17 17:52:00 -0500

    set backend conection

--- src/constants/apiUrls.ts
+++ src/constants/apiUrls.ts
@@ -1,5 +1,5 @@
 class apiUrls {
-  static readonly BASE_URL = "http://localhost:8080/api/v1";
+  static readonly BASE_URL = "http://35.184.160.92:30022/api/v1";
   static readonly GET_USERS_TO_APPROVE_URL = `${this.BASE_URL}/users`;
   static readonly LOG_IN_URL = `${this.BASE_URL}/login`
 }
